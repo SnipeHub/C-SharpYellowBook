8fc20aabf60b29df3024bdb013f0c1fabaa15924
SnipeHub <v-macar2@microsoft.com>
2018-06-10 12:23:01 +1000

    If block statements Pg37

---
+++ BlockStatements/BlockStatements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        // Anything between { and } is regarded as a single statement. Pg 37 (42)
+        {
+            double width = 5.1;
+            if (width > 5.0)
+            {
+                Console.WriteLine("Width too big, using maximum\n");
+            }
+
+            else
+            {
+                Console.WriteLine("Width is smaller, using default\n");
+            }
+        }
+    }
+}
