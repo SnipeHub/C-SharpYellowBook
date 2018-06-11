633157df7ebe304414f1a98ed6d0eca1613256e1
SnipeHub <v-macar2@microsoft.com>
2018-06-11 12:00:14 +1000

    while loop

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 1;
+
+            while (i < 11) // While loop
+            {
+                Console.WriteLine("Run this code...");
+                i = i + 1;
+            }
+        }
+    }
+}
