c9efe5e261cd31e62877682370234e0535288f49
SnipeHub <v-macar2@microsoft.com>
2018-06-11 12:30:07 +1000

    ForLoop

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            for (setup; finish test; update)
+            {
+                things we want to do args given
+                number of times
+            }
+            */
+
+            int i;
+
+            for (i = 1; i < 11; i = i + 1) // Better to use a for loop than a while loop, much more simpler.
+            {
+                Console.WriteLine("For loop...");
+            }
+        }
+    }
+}
