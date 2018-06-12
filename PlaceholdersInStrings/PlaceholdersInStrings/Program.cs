c506e47fcc6376f7b3876fb1c638642fde20d2d5
SnipeHub <v-macar2@microsoft.com>
2018-06-12 13:52:34 +1000

    Using Placeholders  example: i: {0} f: {1} etc,

---
+++ PlaceholdersInStrings/PlaceholdersInStrings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaceholdersInStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 150;
+            double f = 1234.56789;
+
+            double r = 650.10;
+            double p = 75984;
+
+            Console.WriteLine("i: {0} f: {1}", i, f); // These are known as placeholders
+            Console.WriteLine("f: {0:0.00} i: {1:0} r: {2:0.00}", f, i, r); // adding number precision 0 and 0.00
+            Console.WriteLine("f: {0:00000.00} r: {1:0000}", f, r); // here we specify the number of printed digits
+            Console.WriteLine("i: {0:#,##0} f: {1:##,##0.00}", i, f); // A # character means put a digit here if you have one
+
+            Console.WriteLine("i: {0,10:0} f: {1,15:0.00}", i, f); // This is good for doing columns, by displaying the numbers 10 units across
+            Console.WriteLine("r: {0,10:0} p: {1,-15:0.00}", 0, 0); // We can also use negative, just put - in front
+            // This also works with strings, so if you want to print columns of words you can use this technique to do it
+
+
+        }
+    }
+}
