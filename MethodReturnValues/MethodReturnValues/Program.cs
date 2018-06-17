68a7dc9c45fa35ab65e88a3fa520b0d0c705afe7
SnipeHub <v-macar2@microsoft.com>
2018-06-17 19:37:30 +1000

    Method with a return value

---
+++ MethodReturnValues/MethodReturnValues/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodReturnValues
+{
+    class Program
+    {
+        static int sillyReturnPlus(int i) // Our method()
+        {
+            i = i + 1;
+            Console.WriteLine("i is: " + i);
+            return i;
+        }
+
+        static void Main(string[] args)
+        {
+            int res;
+            res = sillyReturnPlus(5);
+            Console.WriteLine("res is: " + res);
+        }
+    }
+}
