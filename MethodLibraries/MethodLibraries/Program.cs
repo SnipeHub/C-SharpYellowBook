56dfbc663d1416a7a9e17480d85749ba7511581e
SnipeHub <v-macar2@microsoft.com>
2018-06-21 20:16:37 +1000

    Method libraries part 3.1.10

---
+++ MethodLibraries/MethodLibraries/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodLibraries // 3.1.10
+{
+    class Program
+    {
+        static string readString(string prompt)
+        {
+            string result;
+
+            do
+            {
+                Console.WriteLine(prompt);
+                result = Console.ReadLine();
+            }
+            while (result == "");
+            return result;
+        }
+
+        static int readInt(string prompt, int low, int high)
+        {
+            int result;
+
+            do
+            {
+                string intString = readString(prompt);
+                result = int.Parse(intString);
+            }
+            while ((result < low) || (result > high));
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string name;
+            name = readString("Enter your name: ");
+            Console.WriteLine("Name: " + name);
+
+            int age;
+            age = readInt("Enter your age: ", 0, 100);
+            Console.WriteLine("Age: " + age);
+        }
+    }
+}
