f33944c1c4b79b3bda7d8e137a2a64d42b8932e1
SnipeHub <v-macar2@microsoft.com>
2018-06-21 20:36:32 +1000

    part 3.2.1

---
+++ LocalVarible/LocalVarible/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalVarible
+{
+    class Program
+    {
+        static void OtherMethod()
+        {
+            Local = 99; // this will not compile
+        }
+
+        static void Main(string[] args)
+        {
+            int local = 0;
+            Console.WriteLine("Local is: " + local);
+        }
+    }
+}
---
+++ MemberVariables/MemberVariables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberVariables
+{
+    class Program
+    {
+        // the variable member is part of the class
+        static int member = 0;
+
+        static void OtherMethod() // static must be used, to state that it is always there.
+        {
+            member = 99;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Member is: " + member); 
+            OtherMethod();
+            Console.WriteLine("Member is now: " + member);
+        }
+    }
+}
